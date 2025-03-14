0f00f5408a347c0825fa5b0aed18ba4cb977c8d3
Sebastian Bernauer <sebastian.bernauer@stackable.tech>
2025-03-14 15:19:56 +0100

    chore: Update default.nix to include details for unstable DPDK

--- default.nix
+++ default.nix
@@ -1,16 +1,17 @@
-{ nixpkgs ? import <nixpkgs> {} }:
+{
+  nixpkgs ? import <nixpkgs> {},
+  nixpkgsUnstable ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {},
+}:
 
 nixpkgs.mkShell {
   buildInputs = [
     nixpkgs.pkg-config
     nixpkgs.dpdk
+    # nixpkgsUnstable.dpdk # Uncomment to use dpdk from nixpkgs-unstable
     nixpkgs.imagemagick
     nixpkgs.python312Packages.pyelftools # needed for dpdk-pmdinfo.py
 
     # For debugging
     nixpkgs.gdb
   ];
-
-  # LIBCLANG_PATH = "${nixpkgs.libclang.lib}/lib";
-  # LIBVNCSERVER_HEADER_FILE = "${nixpkgs.libvncserver.dev}/include/rfb/rfb.h";
 }
